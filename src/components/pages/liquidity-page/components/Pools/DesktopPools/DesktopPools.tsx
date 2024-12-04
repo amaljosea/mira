07277f2c58791ef6844efe8b7ecfe065d7028967
amaljosea <amaljoseami@gmail.com>
2024-12-04 11:52:35 +0530

    Added Sort UI

--- src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPools.tsx
+++ src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPools.tsx
@@ -1,9 +1,11 @@
 import styles from "./DesktopPools.module.css";
 import { useRouter } from "next/navigation";
 import { clsx } from "clsx";
-import {PoolData} from "@/src/hooks/usePoolsData";
-import {useCallback} from "react";
+import { PoolData } from "@/src/hooks/usePoolsData";
+import { useCallback, useState } from "react";
 import DesktopPoolRow from "./DesktopPoolRow";
+import { ArrowDownSmallIcon } from "@/src/components/icons/ArrowDown/ArrowDownSmallIcon";
+import { ArrowUpSmallIcon } from "@/src/components/icons/ArrowUp/ArrowUpSmallIcon";
 
 type Props = {
   poolsData: PoolData[] | undefined;
@@ -11,6 +13,18 @@ type Props = {
 
 const DesktopPools = ({ poolsData }: Props) => {
   const router = useRouter();
+
+  const [sortConfig, setSortConfig] = useState({
+    key: "TVL",
+    direction: "DESC",
+  });
+
+  const handleSort = (key: string) => {
+    setSortConfig((prev) => ({
+      key,
+      direction: prev.key === key && prev.direction === "DESC" ? "ASC" : "DESC",
+    }));
+  };
 
   if (!poolsData) {
     return null;
@@ -21,9 +35,24 @@ const DesktopPools = ({ poolsData }: Props) => {
       <thead>
         <tr>
           <th>Pools</th>
-          <th>APR</th>
-          <th>24H Volume</th>
-          <th>TVL</th>
+          <SortableColumn
+            title="APR"
+            columnKey="APR"
+            sortConfig={sortConfig}
+            onSort={handleSort}
+          />
+          <SortableColumn
+            title="24H Volume"
+            columnKey="Volume"
+            sortConfig={sortConfig}
+            onSort={handleSort}
+          />
+          <SortableColumn
+            title="TVL"
+            columnKey="TVL"
+            sortConfig={sortConfig}
+            onSort={handleSort}
+          />
           <th>
             {/*<ActionButton*/}
             {/*className={styles.createButton}*/}
@@ -40,6 +69,38 @@ const DesktopPools = ({ poolsData }: Props) => {
         ))}
       </tbody>
     </table>
+  );
+};
+
+type SortableColumnProps = {
+  title: string;
+  columnKey: string;
+  sortConfig: { key: string; direction: string };
+  onSort: (key: string) => void;
+};
+
+const SortableColumn = ({
+  title,
+  columnKey,
+  sortConfig,
+  onSort,
+}: SortableColumnProps) => {
+  const isActive = sortConfig.key === columnKey;
+
+  return (
+    <th onClick={() => onSort(columnKey)} className={styles.sortable}>
+      <div className={styles.sortArea}>
+        {title}
+        <span className={styles.sortIcon}>
+          {isActive &&
+            (sortConfig.direction === "DESC" ? (
+              <ArrowDownSmallIcon />
+            ) : (
+              <ArrowUpSmallIcon />
+            ))}
+        </span>
+      </div>
+    </th>
   );
 };
 
