19a459d86f8d0c4408006b71b641e61b04b556b0
amaljosea <amaljoseami@gmail.com>
2024-12-05 02:48:01 +0530

    fix pagination UI

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -13,52 +13,39 @@ const Pagination = ({
 }: PaginationProps) => {
   const generatePages = () => {
     const pages = [];
-    const maxVisiblePages = 5; // Max number of visible page buttons (including ellipsis)
+    const maxVisiblePages = 5;
 
     if (totalPages <= maxVisiblePages) {
-      // If total pages fit within maxVisiblePages, show all pages
       for (let i = 1; i <= totalPages; i++) {
         pages.push(i);
       }
     } else {
-      // Always show the first and last pages
       pages.push(1);
+      const startPage = Math.max(2, currentPage - 1);
+      const endPage = Math.min(totalPages - 1, currentPage + 1);
 
-      const startPage = Math.max(2, currentPage - 1); // Start range
-      const endPage = Math.min(totalPages - 1, currentPage + 1); // End range
-
-      // Add left truncation
       if (startPage > 2) {
         pages.push("...");
       }
 
-      // Add the middle range of pages
       for (let i = startPage; i <= endPage; i++) {
         pages.push(i);
       }
 
-      // Add right truncation
       if (endPage < totalPages - 1) {
         pages.push("...");
       }
 
-      // Add the last page
       pages.push(totalPages);
     }
 
     return pages;
   };
 
-  const handlePageClick = (page: number | string) => {
-    if (typeof page === "number") {
-      onPageChange(page);
-    }
-  };
-
   return (
     <div className={styles.pagination}>
       <button
-        className={`${styles.paginationButton} ${styles.previousButton}`}
+        className={`${styles.paginationButton} ${styles.previous}`}
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
       >
@@ -70,14 +57,14 @@ const Pagination = ({
           className={`${styles.paginationButton} ${
             page === currentPage ? styles.active : ""
           }`}
-          onClick={() => handlePageClick(page)}
+          onClick={() => typeof page === "number" && onPageChange(page)}
           disabled={page === "..."}
         >
           {page}
         </button>
       ))}
       <button
-        className={`${styles.paginationButton} ${styles.nextButton}`}
+        className={`${styles.paginationButton} ${styles.next}`}
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
       >
