f8029c256f99e371d64c20afca3826cf03e7b2c5
Vinu Cyril <vinucyril@gmail.com>
2025-04-18 00:05:20 +0530

    feat: implement multi-breakpoint positioning (70% mobile, 40% tablet, 30% desktop) for Dino

--- src/components/common/GlitchEffects/GlitchEffects.tsx
+++ src/components/common/GlitchEffects/GlitchEffects.tsx
@@ -18,12 +18,7 @@ const GlitchEffects = ({overlayContent}: Props) => (
       <div className="rainbowColor rainbowColor4"></div>
       <div className="rainbowColor rainbowColor5"></div>
     </div>
-    <Image
-      src={dino}
-      alt="Derek Dino"
-      className="dino"
-      style={{left: "30%", width: "190px", height: "170px"}}
-    />
+    <Image src={dino} alt="Derek Dino" className="dino" />
     <MicroChainStatusText />{" "}
   </>
 );
